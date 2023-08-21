089d33d0c1fdc63ca134d311ebf3b8907284bce0
treaddy <awjd06@gmail.com>
2023-08-21 22:06:07 +1000

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -10,11 +10,10 @@ addLineSensor(0, 0, 255, 0, 0); //8 orange sensor3 - red component
 addLineSensor(0, 0, 0, 255, 0); //9 orange sensor4 - non-red component
 addLineSensor(22, 0, 0, 0, 255); //10 blue sensor - blue component
 addLineSensor(22, 0, 255, 255, 0); //11 blue sensor - non-blue component
-addLineSensor(28, 0, 255, 255, 255); //12 line sensor
+addLineSensor(28, 0, 255, 255, 255); //12 line finding sensor
 addLineSensor(6, -13, 255, 0, 255); //13 far left junction
 addLineSensor(6, 13, 255, 0, 255); //14 far right junction
 addLineSensor(5, 0, 255, 0, 255); //15 middleFwd
-//console.log(readSensor(12))
 
 setColour("deepskyblue");
 setThickness(13);
@@ -24,11 +23,12 @@ insidePerimeter = false;
 searchPattern = false;
 perimeterFound = false;
 
-var penCount = 2
+var penCount = 2 //this is to prevent the bot from restarting the orange zone sequence, counts down after each pen use per zone
 
 function isOrange() {
     var red = readSensor(6);
-    var green = readSensor(7); // orange is more red than green
+    var green = readSensor(7); 
+	//found the original calculation would treat the red stop as orange so had to add a minimum green which greatly reduces false hits
     if (red > 0.4 && green < 0.9 * red && green > 0.2)  {
         return true;
     }
@@ -38,11 +38,10 @@ function isOrange() {
 }
 
 function isBlue() {
+	//similar function for is orange, and helps the bot avoid if it encounters blue for zone entry and survivor 
     var blue = readSensor(10);
     var redgreen = readSensor(11);
     if (blue > 0.9 && redgreen < 0.4 * blue) {
-        //console.log("blue is " + blue);
-		//console.log("rg is " + redgreen);
 		return true;
     }
     else {
@@ -50,11 +49,10 @@ function isBlue() {
     }
 }
 
-
-
 function isOrangeExit() {
+	//similar to isOrange but uses sensors further ahead to find orange zone boundary
     var red = readSensor(8);
-    var green = readSensor(9); // orange is more red than green
+    var green = readSensor(9);
     if (red > 0.4 && green < 0.9 * red && green > 0.2) {
         return true;
     }
@@ -63,24 +61,11 @@ function isOrangeExit() {
     }
 }
 
-function boxBoundary() {
-    var l = readSensor(0) > 0.5; // left seeing light
-    var r = readSensor(1) > 0.5; // right seeing light
-    var red = readSensor(8);
-    var green = readSensor(9);
-    var boundaryLow = readSensor(6) > 0.93;
-    var boundaryHigh = readSensor(7) === 0.96; // orange is more red than green
-    if (boundaryLow && boundaryHigh) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 
 function isSurvivor() {
+	//isSurvivor code from assignment samples - nil changes
     var red = readSensor(6);
-    var green = readSensor(7); // The survivor colour has a lot of green in it and not much red
+    var green = readSensor(7);
     if (green > 0.4 && red < 0.9 * green) {
         return true;
     }
@@ -120,53 +105,13 @@ function survivorExit() {
             var lineSens = readSensor(6) < 0.3;
             var boundaryLow = readSensor(6) > 0.93;
             var boundaryHigh = readSensor(7) < 0.96;
-			//find direction to go
-			
-			if (l && !r) {
-				//left outward
-				console.log("left outward")
-				var leftOut = false
-				while (!leftOut) {
-					var boundaryLow = readSensor(6) > 0.9;
-					var boundaryHigh = readSensor(7) < 0.98;
-					var l = readSensor(0) > 0.5; // left seeing light
-					var r = readSensor(1) > 0.5; // right seeing light	
-
-					if (boundaryLow && boundaryHigh) {
-						forward(1);
-					}
-					else if (isBlue()) {
-						forward(30)
-					}
-					else if (lineSens){
-						console.log("found exit")
-						right(30)
-						forward(15)
-						leftOut = true
-					}
-					else {
-						if (!l) {
-							left(4);
-						}
-						else if (r) {
-							right(4);
-						}
-
-						else {
-							forward(1)
-						}
-					}
-				lineFound = true;
-				}
-			}
-		
-			
+			//find edge
+						
 			if (!l && r){
-				//right outward
-				console.log("right outward")
 				var rightOut = false
+				//this loop finds the edge/boundary of the orange zone, then uses boundary high and low used to measure the boundary of the orange zone and maintain a line, approaching from the right until it finds the exit
 				while (!rightOut) {
-					var boundaryLow = readSensor(6) > 0.9;
+					var boundaryLow = readSensor(6) > 0.9;  
 					var boundaryHigh = readSensor(7) < 0.97;
 					var l = readSensor(0) > 0.53; // left seeing light
 					var r = readSensor(1) > 0.53; // right seeing light
@@ -225,6 +170,7 @@ function survivorSearch() {
             forward(1);
 			right(180);
             penUp();
+			// makes pen count zero to prevent restarting orange zone
 			var penCount = 0
             forward(25);
         }
@@ -232,34 +178,24 @@ function survivorSearch() {
 			left(180);
 		}
         else {
+			//found that having only two values for left and right turns would lead the bot to get stuck in corners
+			//made an array to select from three angles at random while still using the toggle
             const anglesArray = [120, 150, 220];
             var randomAngle = Math.floor(Math.random() * anglesArray.length);
             toggle = !toggle;
             if (toggle) {
-                right(anglesArray[randomAngle]); //console.log(anglesArray[randomAngle])
+                right(anglesArray[randomAngle]); 
             }
             else {
-                left(anglesArray[randomAngle]); //console.log(anglesArray[randomAngle])
+                left(anglesArray[randomAngle]); 
             }
         }
     }
 }
 
-function findLine() {
-    lineLost = true;
-    var lTurnCount = 4;
-    var rTurnCount = 10;
-    println("Path Lost");
-}
-
-function linePresent(){
-	
-}
-
-
 function followPath() {
     while (!stop) {
-		var l = (readSensor(0) > 0.71);// left seeing light
+		var l = (readSensor(0) > 0.71);// left seeing light - bumped up the value to make the path finding more rigid
         var r = (readSensor(1) > 0.71); // right seeing light
 		var lHigh = readSensor(0) < 0.99; // left seeing light
         var rHigh = readSensor(1) < 0.99; // right seeing light
@@ -267,24 +203,23 @@ function followPath() {
         var jRight = readSensor(3) < 0.5; // if this goes dark, turn right
 		var jLeft1 = readSensor(2) > 0.85; // if this goes dark, turn right
         var jRight1 = readSensor(3) > 0.85; // if this goes dark, turn right
-		var middleSens = readSensor(4) > 0.7; //middle sensor if total white
-        var oSens1 = readSensor(6);
-        var oSens2 = readSensor(7);
-		var middleFarSens = readSensor(7) > 0.5;
-		var lineSens = readSensor(12) > 0.85; // seeing light
-		var lFar = readSensor(13) < 0.68; // left seeing light
-        var rFar = readSensor(14) < 0.68; // right seeing light
-		var middleFwdSens = readSensor(15) > 0.8; //middle sensor if total white
-		//console.log("lineSens is " + readSensor(12) + " lFar is " + readSensor(13) + " rFar is " + readSensor(14))
-		//console.log("linesense" + readSensor(12) + " rFar is " + readSensor(14))
+        var oSens1 = readSensor(6); //orange zone red
+        var oSens2 = readSensor(7); // orange zone green
+		var middleFarSens = readSensor(7) > 0.5; //further middle sensor looking for gaps
+		var lineSens = readSensor(12) > 0.85; // detecting if the pathway line has been lost for other var/conditional comparisons
+		var lFar = readSensor(13) < 0.68; // left - further sensors for middle and middleFar sensor comparison
+        var rFar = readSensor(14) < 0.68; // right - further sensors for middle and middleFar sensor comparison
+		var middleFwdSens = readSensor(15) > 0.8; //middle sensor looking for gaps
+
 		stop = readSensor(4) > 0.69 && readSensor(5) < 0.12
 		
 		if (readSensor(4) > 0.69 && readSensor(5) < 0.12) {
+			//the calculation has been identified within .1 of a decimal to get the exact red zone colour
 			stop = true;
 		}
         else if (l && r && !lineSens) {
+			//added the linesens detection to help enforce else if conditions listed below
             forward(3);
-			//console.log(l);
         }
         else if (l && !r) {
             right(4);
@@ -295,16 +230,17 @@ function followPath() {
             forward(0.8);
         }
         else {
+			//green junction turns, added the opposite var for each condition to make them more explicit for turning
             if (jLeft && !jRight) {
                 forward(1);
-                left(9); //console.log("found juncleft")
+                left(9);
             }
             else if (jRight && !jLeft) {
                 forward(1);
-                right(9); //console.log("found juncright")
+                right(9);
             }
             else if (oSens1 > 0.4 && oSens2 < 0.9 * oSens1 && oSens2 > 0.2) {
-                //console.log("Found survivor Zone");
+                //if pencount is 2, it is assumed the zone has not been encountered yet
 				if(penCount = 2) {
 					penDown();
 					forward(6);
@@ -316,15 +252,14 @@ function followPath() {
 					survivorExit();
 				}
 				var penCount = 2
+				//pencount reset after exiting successfully
             }
 			else if (middleFwdSens) {
-				//console.log("middleSens trigger")
+				//condition to assist with navigating the segmented semi-circle paths
 				if (lFar) {
-					//console.log("lhighlfar trigger");
 					left(8);
 				}			
 				else if (rFar) {
-					//console.log("rhighrfar trigger");
 					right(8);	
 				}
 				else {
@@ -332,14 +267,14 @@ function followPath() {
 				}
 			}
 			else if (middleFarSens && !middleFwdSens) {
-				//console.log("farSens trigger")
+				//condition to navigate the zigzag, depending on the wobble this can go very smoothly/slowly
 				if (lFar && !jRight1) {
-					//console.log("lhighlfar trigger");
+
 					left(21);
 					forward(0.01);
 				}			
 				else if (rFar && !jLeft1) {
-					//console.log("rhighrfar trigger");
+
 					right(21);
 					forward(0.01);
 				}
